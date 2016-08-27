0f134150b69b26a7c3e3178f77667cdb410cbb15
Brooke Peterson <brookezpeterson@gmail.com>
2016-08-26 23:38:35 -0700

    First pass on several responsive breakpoints ..

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Some thoughts on how I did this
+# This exaplins how I did things on this single-page site.
+
+## workflow:
+1. included compass so I can use the compass reset and font-face
+
+## Using svg 
+1. decide to use svg as a background image which do not need to resize it, can just use the backgroud-size to get the correct size.
+2. There are enough headings on the page for SEO purposes so do not need to use the logo as an img.
+3. For mobile users can prevent them copying the svg, otherwsie when user touch the image it will pop up a dialog box asking if the users what to save the images
+4. Created white version and grey version of the log svg files simply chaged the fill color using sublime text
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp  =  require('gulp'),
 	gutil = require('gulp-util'),
-	compass = require('gulp-compass');
+	compass = require('gulp-compass'),
+	connect = require('gulp-connect');
 
 
 var sassFiles = ['app/sass/drcstyle.scss']
@@ -18,4 +19,28 @@ gulp.task('compass', function(){
 	}))
 	.on('error', gutil.log)
 	.pipe(gulp.dest('app/css'))
+	.pipe(connect.reload())
+});
+
+gulp.task('html', function() {
+  gulp.src('app/*.html')
+    // .pipe(gulpif(env === 'production', minifyHTML()))
+    // .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/sass/*.scss', ['compass']);
+	gulp.watch('app/*.html', ['html']);
+});
+gulp.task('connect', function(){
+	connect.server({
+		root: 'app/',
+		livereload:true
+	})
 })
+
+
+
+
+gulp.task('default', ['compass', 'connect','watch'])
